23d2c3b0ad8cb780a84a969263dc2ebfa2477001
Gio Lodi <giovanni.lodi42@gmail.com>
2021-07-02 06:18:49 +1000

    Track template app project (App) generated by Xcode 13 beta 2

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Gio on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
