4e06e5a916c9305049ea985d2c267a2cdc064014
Gio Lodi <giovanni.lodi42@gmail.com>
2021-07-02 06:30:35 +1000

    Step 01 – Show user greetings implemented inline with no tests

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -1,14 +1,31 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // With `private(set) var` + default value, the compiler will
+    // generated an init but we'll also be able to call `.init()`
+    // with no arguments, meaning there's less code to change.
+    // This is useful for faster feedback, but you might want to
+    // remove the default value unless it makes sense to have one
+    // after you're done iterating on the implementation.
+    private(set) var userName: String? = .none
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if let userName = userName {
+            Text("Hello, \(userName)!")
+                .padding()
+        } else {
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        // Using two different previews to visually validate
+        // both behaviors.
+        ContentView(userName: "Ada")
         ContentView()
     }
 }
