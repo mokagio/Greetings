3869cf99ff54107be3a3b308c1d9054ffff8e34f
Gio Lodi <giovanni.lodi42@gmail.com>
2021-07-03 13:15:16 +1000

    Step 04.a – Add test for user name interpolation salutation – Test fails

--- GreetingsTests/GreetingsTests.swift
+++ GreetingsTests/GreetingsTests.swift
@@ -14,7 +14,11 @@ class GreetingsTests: XCTestCase {
         XCTAssertEqual(greeting, "G'day, mate!")
     }
 
-    func testGreetingsWithUserNameSaysGdayUserName() {}
+    func testGreetingsWithUserNameSaysGdayUserName() {
+        // Arrange, Act, Assert is an excellent way to organize a test, but it's also good to be
+        // concise if you can
+        XCTAssertEqual(greetings(userName: "Ada"), "G'day, Ada!")
+    }
 }
 
 // I like to start working on code in the same file as the test, so I don't have to move between
